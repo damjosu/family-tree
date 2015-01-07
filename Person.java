4b106631112af3b47b8d1553067347b51b7726d0
BuildTools <unconfigured@null.spigotmc.org>
2015-01-07 19:38:35 +0100

    showData, setFather, setMother and addChild methods created

--- Person.java
+++ Person.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 /**
- * Person class
+ * Person class.
  */
 public class Person
 {
@@ -7,6 +8,12 @@ public class Person
     private String name;
     // The age of the person.
     private int age;
+    // Person's father.
+    private Person father;
+    // Person's Mother.
+    private Person mother;
+    // Person's children/s.
+    private ArrayList<Person> children;
     
     /**
      * Person's constructor, which you can set the person's name and age.
@@ -16,4 +23,38 @@ public class Person
         this.name = name;
         this.age = age;
     }
+    
+    /**
+     * Prints the person's name and age.
+     */
+    public void showData()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    
+    /**
+     * Sets the person's father.
+     */
+    public void setFather(Person father)
+    {
+        this.father = father;        
+    }
+    
+    /**
+     * Sets the person's mother.
+     */
+    public void setMother(Person mother)
+    {
+        this.mother = mother;
+    }
+    
+    /**
+     * Sets the person's child/s.
+     */
+    public void addChild(Person child)
+    {
+        children.add(child);
+    }
+ 
 }
