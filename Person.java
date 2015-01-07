f1dc166b3638246a5ac895a17781da5496a6c9a3
BuildTools <unconfigured@null.spigotmc.org>
2015-01-07 18:38:12 +0100

    Person's class and constructor created

---
+++ Person.java
@@ -0,0 +1,19 @@
+/**
+ * Person class
+ */
+public class Person
+{
+    // The name of the person.
+    private String name;
+    // The age of the person.
+    private int age;
+    
+    /**
+     * Person's constructor, which you can set the person's name and age.
+     */
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
